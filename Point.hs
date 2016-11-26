60668986aee6dd7018fd39efeb09d765147952ef
stwykd <stwykd@gmail.com>
2016-11-26 17:59:29 +0000

    Remove refactor a bit

--- Point.hs
+++
@@ -1,4 +0,0 @@
-data Point = Point Double Double deriving Show
-data Point = Cartesian Double Double
-           | Polar Double Double
-             deriving Show
--- baby.hs
+++
@@ -1,2 +0,0 @@
-doubleMe x = x + x
-doubleUs x y = doubleMe x + doubleMe y
