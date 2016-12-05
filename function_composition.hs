eb377bf8910223f4e93051966074273f6b49a43a
pierluigi urru <pierluigi.urru@thomsonreuters.com>
2016-12-05 08:12:56 +0000

    Remove .hs files

--- function_composition.hs
+++
@@ -1,9 +0,0 @@
-import Data.Char    -- brings function isLower into scope
-
-countLowerCase :: String -> Int
-countLowerCase str = length (filter isLower str)
-
--- ... with function composition, f (g x) becomes (f . g) x, runs in constant space
--- Doesn't name the argument, style called point-free
-countLowerCase :: String -> Int
-countLowerCase = length . filter isLower
